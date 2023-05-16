e44954628ac65a9520a655f26ba0784933c99a40
HanSangWook <qazws78941@gmail.com>
2023-05-16 10:07:09 +0900

    가장 기본적인 카운팅 앱

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,23 +1,41 @@
 import 'package:flutter/material.dart';
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
   const App({super.key});
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  int count = 0;
+
+  void increase() => setState(() {
+        count++;
+      });
+
+  void decrease() => setState(() {
+        count--;
+      });
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text(
-              '0',
-              style: TextStyle(fontSize: 40),
+            Text(
+              '$count',
+              style: const TextStyle(fontSize: 40),
             ),
             Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
-                ElevatedButton(onPressed: () {}, child: const Icon(Icons.add)),
                 ElevatedButton(
-                    onPressed: () {}, child: const Icon(Icons.remove)),
+                    onPressed: increase, child: const Icon(Icons.add)),
+                ElevatedButton(
+                    onPressed: decrease, child: const Icon(Icons.remove)),
               ],
             )
           ],
